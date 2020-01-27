10b825eefc2ac3dfbf684fa7d8c62784b7c0835c
kbbinghai <1617079905@qq.com>
2020-01-27 16:05:55 +0800

    修改isskongzhen

---
+++ src/main/scala/com/xiaoniu/dmp/report/GitTest.scala
@@ -0,0 +1,17 @@
+package com.xiaoniu.dmp.report
+
+/**
+  * date  2020/1/27-16:03
+  * author zhaishuai
+  * Description: 
+  * 运行的结果：
+  */
+object GitTest {
+  
+  def main(args: Array[String]): Unit = {
+    
+    println("===你是一个小人物===")
+    
+  }
+  
+}
